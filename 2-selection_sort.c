80856107e6900f6bc8baec907e7463b339900b8f
OTMANE <davenamir3@gmail.com>
2024-02-21 01:40:11 +0100

    test

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,36 +1,35 @@
 #include "sort.h"
 
 /**
- * selection_sort - func
- * @array: inputs
- * @size: in
-*/
-
+ * selection_sort - sorts an array of integers in ascending order using
+ *                  the Selection sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ */
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, k;
-	int temp;
+    size_t i, j, min_idx;
+    int temp;
 
-	if (!array || !size)
-		return;
+    if (!array || size < 2)
+        return;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		for (j = size - 1; k = i + 1; j--)
-		{
-			if (array[j] < array[k])
-			{
-				k = j;
-			}
-		}
+    for (i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+                min_idx = j;
+        }
 
-		if (array[j] < array[k])
-		{
-			temp = array[i];
-			array[i] = array[k];
-			array[k] = temp;
-			print_array(array, size);
-		}
-	}
+        if (min_idx != i)
+        {
+            temp = array[i];
+            array[i] = array[min_idx];
+            array[min_idx] = temp;
+            print_array(array, size);
+        }
+    }
 }
